b1e83c0e0b78a00a84742f6952bf910fe32edd10
perhje <38944777+perhje@users.noreply.github.com>
2018-10-09 16:22:25 +0200

    cfdtyjy

--- app/src/main/java/com/example/phj_1/s305068s315303mappe2/MainActivity.java
+++ app/src/main/java/com/example/phj_1/s305068s315303mappe2/MainActivity.java
@@ -1,11 +1,12 @@
 package com.example.phj_1.s305068s315303mappe2;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
