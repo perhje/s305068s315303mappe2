8c9abc07f6d2e717b992198ac8cc81933c6d5633
Nguyen pham <phamducnguyen@hotmail.com>
2018-10-09 15:58:18 +0200

    first commmit

--- app/src/main/java/com/example/phj_1/s305068s315303mappe2/MainActivity.java
+++ app/src/main/java/com/example/phj_1/s305068s315303mappe2/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.phj_1.s305068s315303mappe2;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +12,20 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void showGame(View v){
+        Intent intent=new Intent(this,Venner.class);
+        startActivity(intent);
+    }
+
+    public void showStatistics(View v){
+        Intent intent=new Intent(this,Restaurant.class);
+        startActivity(intent);
+    }
+
+    public void showPreferences(View v){
+        Intent intent=new Intent(this,Bestilling.class);
+        startActivity(intent);
+    }
+
 }
