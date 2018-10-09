ff065c84047c8a71c216fb74a2c7453e87d06647
perhje <38944777+perhje@users.noreply.github.com>
2018-10-09 15:23:11 +0200

    Initial commit

---
+++ app/src/main/java/com/example/phj_1/s305068s315303mappe2/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.phj_1.s305068s315303mappe2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
