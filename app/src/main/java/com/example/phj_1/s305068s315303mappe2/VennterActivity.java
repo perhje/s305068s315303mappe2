c34a497d46713f9a9341514baaa78c4afd5825e3
Nguyen pham <phamducnguyen@hotmail.com>
2018-10-10 12:09:27 +0200

    second

---
+++ app/src/main/java/com/example/phj_1/s305068s315303mappe2/VennterActivity.java
@@ -0,0 +1,27 @@
+package com.example.phj_1.s305068s315303mappe2;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class VennterActivity extends Activity{
+    EditText navni;
+    EditText adressei;
+    EditText telefoni;
+    EditText idi;
+    TextView visvenner;
+    DBHandler db;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.restaurantactivity);
+        navni=(EditText) findViewById(R.id.navn);
+        adressei=(EditText) findViewById(R.id.adresse);
+        telefoni=(EditText) findViewById(R.id.telefon);
+        visvenner=(TextView) findViewById(R.id.visrestaurant);
+        idi= (EditText) findViewById(R.id.restaurant_id);
+    }
+
+    
+}
