032d8fc61a1b51745a5603d5023da50070c30ad7
perhje <38944777+perhje@users.noreply.github.com>
2018-10-10 11:37:43 +0200

    hbjhk jl

--- app/src/main/java/com/example/phj_1/s305068s315303mappe2/DBHandler.java
+++ app/src/main/java/com/example/phj_1/s305068s315303mappe2/DBHandler.java
@@ -1,4 +1,190 @@
 package com.example.phj_1.s305068s315303mappe2;
 
-public class DBHandler {
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+import java.sql.Time;
+import java.util.ArrayList;
+import java.util.List;
+
+public class DBHandler extends SQLiteOpenHelper{
+    static String TABLE_VENNER = "Venner";
+    static String VENNER_ID = "_ID";
+    static String NAVN_VENNER = "Navn";
+    static String TELEFON_VENNER = "Telefon";
+
+    static String TABLE_RESTAURANT = "Restaurant";
+    static String RESTAURANT_ID = "_ID";
+    static String NAVN_RESTAURANT = "Navn";
+    static String ADRESSE_RESTAURANT = "Adresse";
+    static String TELEFON_RESTAURANT = "Telefon";
+    static String TYPE_RESTAURANT = "Type";
+
+    static String TABLE_BESTILLING = "Bestilling";
+    static String BESTILLING_ID = "_ID";
+    static String BESTILLING_TIDSPUNKT = "Tidspunkt";
+    static String BESTILLING_VENNER = "Venner";
+
+    static String DATABASE_NAVN = "Restaurantdatabase";
+    static int DATABASE_VERSJON = 0;
+
+    public DBHandler(Context context){
+        super(context, DATABASE_NAVN, null, DATABASE_VERSJON);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        String LAG_VENNER_TABELL = "CREATE TABLE " + TABLE_VENNER + "(" + VENNER_ID + " INTEGER PRIMARY KEY,"
+                + NAVN_VENNER + " TEXT," + TELEFON_VENNER + " TEXT" + ")";
+        Log.d("SQL", LAG_VENNER_TABELL);
+        String LAG_RESTAURANT_TABELL = "CREATE TABLE " + TABLE_RESTAURANT + "(" + RESTAURANT_ID + " INTEGER PRIMARY KEY,"
+                + NAVN_RESTAURANT + " TEXT," + ADRESSE_RESTAURANT + " TEXT," + TELEFON_RESTAURANT + " TEXT,"
+                + TYPE_RESTAURANT + " TEXT" + ")";
+        Log.d("SQL", LAG_RESTAURANT_TABELL);
+        String LAG_BESTILLING_TABELL = "CREATE TABLE " + TABLE_VENNER + "(" + BESTILLING_ID + " INTEGER PRIMARY KEY,"
+                + BESTILLING_VENNER + " TEXT," + BESTILLING_TIDSPUNKT + " TEXT" + ")";
+        Log.d("SQL", LAG_BESTILLING_TABELL);
+        db.execSQL(LAG_VENNER_TABELL);
+        db.execSQL(LAG_RESTAURANT_TABELL);
+        db.execSQL(LAG_BESTILLING_TABELL);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int i, int i1) {
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_VENNER);
+        Log.d("UPDATE", "i venner");
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_RESTAURANT);
+        Log.d("UPDATE", "i restaurant");
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_BESTILLING);
+        Log.d("UPDATE", "i bestilling");
+        onCreate(db);
+    }
+
+    public void leggTilVenner(Venner venner){
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues values = new ContentValues();
+        values.put(NAVN_VENNER, venner.getNavn());
+        values.put(TELEFON_VENNER, venner.getTelefon());
+        db.insert(TABLE_VENNER, null,values);
+        db.close();
+    }
+
+    public void leggTilRestaurant(Restaurant restaurant){
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues values = new ContentValues();
+        values.put(NAVN_RESTAURANT, restaurant.getNavn());
+        values.put(ADRESSE_RESTAURANT, restaurant.getAdresse());
+        values.put(TELEFON_RESTAURANT, restaurant.getTelefon());
+        values.put(TYPE_RESTAURANT, restaurant.getType());
+        db.insert(TABLE_RESTAURANT, null,values);
+        db.close();
+    }
+
+    public void leggTilBestilling(Bestilling bestilling){
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues values = new ContentValues();
+        values.put(BESTILLING_TIDSPUNKT, bestilling.getTidspunkt());
+        values.put(BESTILLING_VENNER, bestilling.getVenner());
+        db.insert(TABLE_BESTILLING, null,values);
+        db.close();
+    }
+
+    public List<Venner> finnAlleVenner(){
+        List<Venner> vennerListe = new ArrayList<Venner>();
+        String selectQuery = "SELECT * FROM " + TABLE_VENNER;
+        SQLiteDatabase db = this.getWritableDatabase();
+        Cursor cursor = db.rawQuery(selectQuery,null);
+        if (cursor.moveToFirst()){
+            do {
+                Venner venner = new Venner();
+                venner.set_ID(cursor.getLong(0));
+                venner.setNavn(cursor.getString(1));
+                venner.setTelefon(cursor.getString(2));
+                vennerListe.add(venner);
+            }while (cursor.moveToNext());
+            cursor.close();
+            db.close();
+        }
+        return  vennerListe;
+    }
+
+    public List<Restaurant> finnAlleRestauranter(){
+        List<Restaurant> restaurantListe = new ArrayList<Restaurant>();
+        String selectQuery = "SELECT * FROM " + TABLE_RESTAURANT;
+        SQLiteDatabase db = this.getWritableDatabase();
+        Cursor cursor = db.rawQuery(selectQuery,null);
+        if (cursor.moveToFirst()){
+            do {
+                Restaurant restaurant = new Restaurant();
+                restaurant.set_ID(cursor.getLong(0));
+                restaurant.setNavn(cursor.getString(1));
+                restaurant.setAdresse(cursor.getString(2));
+                restaurant.setTelefon(cursor.getString(3));
+                restaurant.setType(cursor.getString(4));
+                restaurantListe.add(restaurant);
+            }while (cursor.moveToNext());
+            cursor.close();
+            db.close();
+        }
+        return  restaurantListe;
+    }
+
+    public List<Bestilling> finnAlleBestilling(){
+        List<Bestilling> bestillingListe = new ArrayList<Bestilling>();
+        String selectQuery = "SELECT * FROM " + TABLE_BESTILLING;
+        SQLiteDatabase db = this.getWritableDatabase();
+        Cursor cursor = db.rawQuery(selectQuery,null);
+        if (cursor.moveToFirst()){
+            do {
+                Bestilling bestilling = new Bestilling();
+                bestilling.set_ID(cursor.getLong(0));
+                bestilling.setTidspunkt(cursor.getString(1));
+                bestilling.setVenner(cursor.getString(2));
+                bestillingListe.add(bestilling);
+            }while (cursor.moveToNext());
+            cursor.close();
+            db.close();
+        }
+        return bestillingListe;
+    }
+
+    public void slettVenner(Long inn_id){
+        SQLiteDatabase db = this.getWritableDatabase();
+        db.delete(TABLE_VENNER, VENNER_ID + " =? ", new String[]{Long.toString(inn_id)});
+        db.close();
+    }
+
+    public void slettRestaurant(Long inn_id){
+        SQLiteDatabase db = this.getWritableDatabase();
+        db.delete(TABLE_RESTAURANT, RESTAURANT_ID + " =? ", new String[]{Long.toString(inn_id)});
+        db.close();
+    }
+
+    public int endreVenner(Venner venner){
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues values = new ContentValues();
+        values.put(NAVN_VENNER,venner.getNavn());
+        values.put(TELEFON_VENNER, venner.getTelefon());
+        int endret = db.update(TABLE_VENNER, values, VENNER_ID + "= ?",
+                new String[]{String.valueOf(venner.get_ID())});
+        db.close();
+        return endret;
+    }
+
+    public int endreRestaurant(Restaurant restaurant){
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues values = new ContentValues();
+        values.put(NAVN_RESTAURANT,restaurant.getNavn());
+        values.put(ADRESSE_RESTAURANT,restaurant.getNavn());
+        values.put(TELEFON_RESTAURANT, restaurant.getTelefon());
+        values.put(TYPE_RESTAURANT,restaurant.getNavn());
+        int endret = db.update(TABLE_RESTAURANT, values, RESTAURANT_ID + "= ?",
+                new String[]{String.valueOf(restaurant.get_ID())});
+        db.close();
+        return endret;
+    }
 }
