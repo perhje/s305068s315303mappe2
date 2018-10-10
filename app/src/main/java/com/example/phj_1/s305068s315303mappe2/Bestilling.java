40f057aeb6f4a6681b2dc45c283cb1bdb6446e3e
perhje <38944777+perhje@users.noreply.github.com>
2018-10-10 14:37:23 +0200

    hbjhk jl

--- app/src/main/java/com/example/phj_1/s305068s315303mappe2/Bestilling.java
+++ app/src/main/java/com/example/phj_1/s305068s315303mappe2/Bestilling.java
@@ -3,10 +3,62 @@ package com.example.phj_1.s305068s315303mappe2;
 import android.app.Activity;
 import android.os.Bundle;
 
-public class Bestilling extends Activity{
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.bestilling);
+public class Bestilling{
+    String Tid;
+    String Restaurant;
+    String Deltakere;
+    long _ID;
+
+    public Bestilling(){
+
+    }
+    public Bestilling(String tid, String restaurant, String deltakere){
+        this.Tid = tid;
+        this.Restaurant = restaurant;
+        this.Deltakere = deltakere;
+    }
+
+    public Bestilling(String tid, String restaurant, String deltakere, long _ID){
+        this.Tid = tid;
+        this.Restaurant = restaurant;
+        this.Deltakere = deltakere;
+        this._ID = _ID;
+    }
+
+    public String getTid() {
+        return Tid;
+    }
+
+    public String getRestaurant() {
+        return Restaurant;
+    }
+
+    public String getDeltakere() {
+        return Deltakere;
+    }
+
+    public long get_ID() {
+        return _ID;
+    }
+
+
+
+    public void set_ID(long _ID) {
+        this._ID = _ID;
+    }
+
+    public void setTid(String tid) {
+
+        Tid = tid;
+    }
+
+    public void setDeltakere(String deltakere) {
+
+        Deltakere = deltakere;
+    }
+
+    public void setRestaurant(String restaurant) {
+
+        Restaurant = restaurant;
     }
 }
