b725e2d0481fdeba36fe821994045158f29b380c
perhje <38944777+perhje@users.noreply.github.com>
2018-10-10 12:02:08 +0200

    hbjhk jl

---
+++ app/src/main/java/com/example/phj_1/s305068s315303mappe2/VennerActivity.java
@@ -0,0 +1,12 @@
+package com.example.phj_1.s305068s315303mappe2;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class VennerActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.venneractivity);
+    }
+}
